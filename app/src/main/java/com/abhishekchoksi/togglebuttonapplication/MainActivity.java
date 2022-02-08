f395c0c1063ca14236b177f91abea23683da686e
032-Abhishek Choksi <19bmiit032@gmail.com>
2022-02-08 20:12:53 +0530

    Toggle Button Application @ 2022 #032AbhishekChoksi. All rights reserved.

--- app/src/main/java/com/abhishekchoksi/togglebuttonapplication/MainActivity.java
+++ app/src/main/java/com/abhishekchoksi/togglebuttonapplication/MainActivity.java
@@ -3,6 +3,9 @@ package com.abhishekchoksi.togglebuttonapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void toggleButtonClickEvent(View view){
+        boolean isEnable = ((ToggleButton) view).isChecked();
+        switch (view.getId()){
+            case R.id.toggleButton:
+                if(isEnable){
+                    Toast.makeText(this, "Light On", Toast.LENGTH_SHORT).show();
+                } else{
+                    Toast.makeText(this, "Light Off", Toast.LENGTH_SHORT).show();
+                }
+        }
     }
 }
